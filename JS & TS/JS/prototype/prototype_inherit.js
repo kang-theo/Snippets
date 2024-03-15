7ff68da89255805d5c60e6b9e0ff0e993a93ef5c
kang-theo <theo.cy.kang@gmail.com>
2024-03-15 22:20:15 +0800

    Prototype and __proto__

--- JS & TS/JS/prototype/prototype_inherit.js
+++ JS & TS/JS/prototype/prototype_inherit.js
@@ -2,9 +2,14 @@ function Animal(name) {
   this.name = name;
 }
 
+// prototype is a property of constructor functions.
 Animal.prototype.sayHello = function () {
   console.log("Hello, I'm " + this.name);
 };
+
+// __proto__ is a property of all objects in JavaScript (except null).
+var cat = new Animal("Whiskers");
+console.log(cat.__proto__ === Animal.prototype); // true
 
 function Cat(name, color) {
   Animal.call(this, name);
